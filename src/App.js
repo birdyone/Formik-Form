2f709cf0d484e49e3b843d4f4d546d0a97777f68
birdyone <catalina@legalbufete.com>
2022-03-04 09:31:43 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React from "react";
+// TODO: import useFormik from formik library
+import {useFormik} from 'formik'
+
+function App() {
+  // TODO: add a const called formik assigned to useFormik()
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit: values => {
+      console.log('form: ', values)
+      window.alert('Login Successful')
+    },
+    validate: values => {
+      let errors = {}
+      let emailErrors = validateEmail(values.email)
+      let passwordErrors = validatePassword(values.password)
+      
+      if(emailErrors) errors.email = emailErrors;
+      if(passwordErrors) errors.password = passwordErrors;
+      console.log('errors: ', errors)
+      return errors
+    }
+  });
+  
+  function validateEmail(email) {
+    return validateFieldHasValue(email) || isEmailFormat(email);
+  }
+
+  function validatePassword(password) {
+    return validateFieldHasValue(password)
+  }
+
+  function validateFieldHasValue(value) {
+    return !value ? 'Field required' : undefined;
+  }
+
+  function isEmailFormat(email) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) ? 
+    undefined : 'Username should be an email'
+  }
+
+  return (
+    <div>
+    <form onSubmit={formik.handleSubmit}>
+      <div>Username</div>
+      <input id="emailField" name="email" type="text" onChange={formik.handleChange} value={formik.values.email}/>
+      {formik.errors.email ? 
+        <div id="emailError" style={{color: 'red'}}>{formik.errors.email}</div> : null}
+
+      <div>Password</div>
+      <input id="pswField" name="password" type="text" onChange={formik.handleChange} value={formik.values.password}/>  
+      {formik.errors.password ? 
+        <div id="pswError" style={{color: 'red'}}>{formik.errors.password}</div>: null}
+
+      <button id="submitBtn" type="submit">Submit</button>    
+  </form>
+      <p>
+        The app is ready! You can proceed with the task instructions. TODO:
+        build you form here.
+      </p>
+    </div>
+  );
+}
+
+export default App;
